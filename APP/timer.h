5f18bc935fb7aaeea6e9b50f01bd2286ea306867
Yuch683 <Ghost06chyu@outlook.com>
2026-01-14 18:02:07 +0800

    Update timer.h

--- APP/timer.h
+++ APP/timer.h
@@ -3,7 +3,8 @@
 
 #include <stdint.h>
 
-int TIME1_COUNT = 0;
+extern volatile uint32_t TIME1_COUNT;
+extern volatile uint8_t adc_capture_flag;
 
 void timer1_Init(uint16_t period);
 void timer1_nvic(void);
